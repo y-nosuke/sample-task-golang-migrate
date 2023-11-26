d59c47399a9d23ab0660b61278626a836d43aaea
nakao yoichi <yoichi@ad.physicist00.jp>
2023-11-26 12:20:50 +0900

    event系のテーブルをe_task_eventsにまとめる

--- migrations/000001_create_tables.down.sql
+++ migrations/000001_create_tables.down.sql
@@ -1,9 +1,3 @@
-DROP TABLE IF EXISTS e_task_uncompleted;
-
-DROP TABLE IF EXISTS e_task_completed;
-
-DROP TABLE IF EXISTS e_task_updated;
-
-DROP TABLE IF EXISTS e_task_registered;
+DROP TABLE IF EXISTS e_task_events;
 
 DROP TABLE IF EXISTS r_tasks;
--- migrations/000001_create_tables.up.sql
+++ migrations/000001_create_tables.up.sql
@@ -12,40 +12,13 @@ CREATE TABLE IF NOT EXISTS `r_tasks` (
 	PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `e_task_registered` (
-	`task_id` BINARY(16) NOT NULL,
-	`title` VARCHAR(255) NOT NULL,
-	`detail` TEXT NULL,
-	`deadline` DATE NULL,
-	`created_by` BINARY(16) NOT NULL,
-	`created_at` DATETIME(6) NOT NULL,
-	PRIMARY KEY (`task_id`),
-	FOREIGN KEY `FK_task_registered_tasks`(`task_id`) REFERENCES r_tasks(id) ON DELETE CASCADE
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
-CREATE TABLE IF NOT EXISTS `e_task_updated` (
-	`task_id` BINARY(16) NOT NULL,
-	`title` VARCHAR(255) NOT NULL,
-	`detail` TEXT NULL,
-	`deadline` DATE NULL,
-	`created_by` BINARY(16) NOT NULL,
-	`created_at` DATETIME(6) NOT NULL,
-	PRIMARY KEY (`task_id`, `created_at`),
-	FOREIGN KEY `FK_task_updated_tasks`(`task_id`) REFERENCES r_tasks(id) ON DELETE CASCADE
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
-CREATE TABLE IF NOT EXISTS `e_task_completed` (
-	`task_id` BINARY(16) NOT NULL,
-	`created_by` BINARY(16) NOT NULL,
-	`created_at` DATETIME(6) NOT NULL,
-	PRIMARY KEY (`task_id`, `created_at`),
-	FOREIGN KEY `FK_task_completed_tasks`(`task_id`) REFERENCES r_tasks(id) ON DELETE CASCADE
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
-CREATE TABLE IF NOT EXISTS `e_task_uncompleted` (
-	`task_id` BINARY(16) NOT NULL,
-	`created_by` BINARY(16) NOT NULL,
-	`created_at` DATETIME(6) NOT NULL,
-	PRIMARY KEY (`task_id`, `created_at`),
-	FOREIGN KEY `FK_task_uncompleted_tasks`(`task_id`) REFERENCES r_tasks(id) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS `e_task_events` (
+     `id` BINARY(16) NOT NULL,
+     `type` VARCHAR(255) NOT NULL,
+     `task_id` BINARY(16) NOT NULL,
+     `data` JSON NOT NULL,
+     `created_by` BINARY(16) NOT NULL,
+     `created_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+     PRIMARY KEY (`id`),
+     FOREIGN KEY `FK_task_events_tasks`(`task_id`) REFERENCES r_tasks(id) ON DELETE CASCADE
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
